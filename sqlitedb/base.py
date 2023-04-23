449ec72bd5f6c857409dda763d4948bf27835e81
Nikhil Badyal <nikhill773384@gmail.com>
2023-04-23 12:43:53 +0530

    🐛 Overwrite ensure_connection instead of _cursor

--- sqlitedb/base.py
+++ sqlitedb/base.py
@@ -1,16 +1,25 @@
 """Custom Database Backend."""
+import time
+
 from django.db.backends.postgresql import base as postgresql_base
 from django.db.utils import OperationalError
 
 
 class DatabaseWrapper(postgresql_base.DatabaseWrapper):
-    def _cursor(self, name: str = None):
-        try:
-            return super()._cursor(name)
-        except OperationalError as e:
-            if "Connection already closed" in str(e):
-                self.close()
-                self.connect()
-                return super()._cursor(name)
-            else:
-                raise e
+    def ensure_connection(self):
+        max_retries = 5
+        delay = 1  # Time delay in seconds between retries
+
+        for attempt in range(1, max_retries + 1):
+            try:
+                super().ensure_connection()
+                break
+            except OperationalError as e:
+                print("Closing and retrying.")
+                if 'SSL connection has been closed unexpectedly' in str(e):
+                    self.close()
+                    if attempt == max_retries:
+                        raise e
+                    time.sleep(delay)
+                else:
+                    raise e
